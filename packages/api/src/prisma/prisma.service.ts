276df2a0b34b120684e136a3c83a775ccb44637d
Linux User <linuxuser@vultr.guest>
2026-08-13 00:39:37 +0800

    feat: setup DB schema and Prisma

---
+++ packages/api/src/prisma/prisma.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // connect is optional in Prisma 5, but good for early failure
+    await this.$connect();
+  }
+}
